7553e711f900e07cf95c733b23a844f178b78105
mazakk94 <mazakk94@gmail.com>
2016-02-15 01:20:43 +0100

    sa tabelki, trzeba zrobic dodawanie glosow, roztrzygniecie gry, wysylanie stanu gry do klientow itp

--- joCirclenX/joCirclenX/server.cpp
+++ joCirclenX/joCirclenX/server.cpp
@@ -8,11 +8,18 @@
 
 using namespace std;
 
+/* INIT GLOBALI */
 WSADATA wsaData;
 SOCKET master, new_socket, s;
-int max_clients = 30, activity, addrlen, i, valread;
+int max_clients = 10;
+int activity, addrlen, i, valread;
 vector<SOCKET> clients(max_clients, 0);
+vector<int> clientsTeams(max_clients, -1);
 struct sockaddr_in server, address; //client;
+
+
+vector<int> team1tab(9, 0);
+vector<int> team2tab(9, 0);
 
 void initServer(){
 	//Windows Socket init
@@ -37,7 +44,29 @@ void initServer(){
 		exit(EXIT_FAILURE);
 	}
 
-	puts("Bind done");
+	puts("Zbindowanie zakonczone pomyslnie");
+}
+
+void setVote(int vote, int team){
+	if (team == 1){
+		team1tab[vote]++;
+	} else { //team == 2
+		team2tab[vote]++;
+	}
+}
+
+void printVotes(){
+	for (int i = 0; i < 9; i++){
+		printf("%d ", team1tab[i]);
+		if ((i - 2) % 3 == 0)
+			printf("\n");
+	}
+	printf("\n\n");
+	for (int i = 0; i < 9; i++){
+		printf("%d ", team2tab[i]);
+		if ((i - 2) % 3 == 0)
+			printf("\n");
+	}
 }
 
 int main()
@@ -47,51 +76,22 @@ int main()
 
 	initServer();
 
-
 	int MAXRECV = 1024;
-	//set of socket descriptors
-	fd_set readfds;
-	//1 extra for null character, string termination
+	fd_set readfds; //deskryptory
 	char *buffer;
 	buffer = (char*)malloc((MAXRECV + 1) * sizeof(char));
 
-	
-
-
-	//Listen to incoming connections
 	listen(master, 3);
 
-	//Accept and incoming connection
-	/*puts("Waiting for incoming connections...");
-
-	c = sizeof(struct sockaddr_in);
-	new_socket = accept(s, (struct sockaddr *)&address, &c);
-	if (new_socket == INVALID_SOCKET) {
-		printf("accept failed with error code : %d", WSAGetLastError());
-	}
-
-	puts("Connection accepted");
-
-	//Reply to client
-	message = "Hello Client , I have received your connection. But I have to go now, bye\n";
-	send(new_socket, message, strlen(message), 0);
-
-	getchar();
-
-	closesocket(s);
-	WSACleanup();
-
-	printf("Hello socket\n");
-	*/
-	puts("Waiting for incoming connections...");
+	printf("Czekam na polaczenia graczy\n");
 
 	addrlen = sizeof(struct sockaddr_in);
 	char tmp[INET_ADDRSTRLEN];
 
 
-	while (TRUE) {
-		//clear the socket fd set
-		FD_ZERO(&readfds);
+	while (1) {
+		
+		FD_ZERO(&readfds); //czyscimy liste deskryptorow
 
 		//add master socket to fd set
 		FD_SET(master, &readfds);
@@ -131,6 +131,7 @@ int main()
 			for (i = 0; i < max_clients; i++) {
 				if (clients[i] == 0){
 					clients[i] = new_socket;
+					clientsTeams[i] = 0;
 					printf("Adding to list of sockets at index %d \n", i);
 					break;
 				}
@@ -149,6 +150,7 @@ int main()
 				//recv does not place a null terminator at the end of the string (whilst printf %s assumes there is one).
 				valread = recv(s, buffer, MAXRECV, 0);
 
+
 				if (valread == SOCKET_ERROR) {
 					int error_code = WSAGetLastError();
 					if (error_code == WSAECONNRESET) {
@@ -158,6 +160,7 @@ int main()
 						//Close the socket and mark as 0 in list for reuse
 						closesocket(s);
 						clients[i] = 0;
+						clientsTeams[i] = -1;
 					} else {
 						printf("recv failed with error code : %d", error_code);
 					}
@@ -169,12 +172,24 @@ int main()
 					//Close the socket and mark as 0 in list for reuse
 					closesocket(s);
 					clients[i] = 0;
+					clientsTeams[i] = -1;
 				}
 
 				//Echo back the message that came in
 				else {
 					//add null character, if you want to use with printf/puts or other string handling functions
 					buffer[valread] = '\0';
+					if (clientsTeams[i] == 0){		 // gracz nie wybral druzyny
+						int team = (int)buffer[0] - (int)48;
+						//printf("(int)buffer[0] - 48 = %d \n", team);
+						if (team == 1 || team == 2){ //
+							clientsTeams[i] = team;
+							printf("clientsTeams[%d] = %d \n", i, team);
+						} else {
+							printf("sizeof buffer = %d, %s \n", sizeof(buffer), buffer);
+						}
+					}
+						
 					printf("%s:%d - %s \n", inet_ntop(AF_INET, &(address.sin_addr), tmp, INET_ADDRSTRLEN), ntohs(address.sin_port), buffer);
 					send(s, buffer, valread, 0);
 				}
